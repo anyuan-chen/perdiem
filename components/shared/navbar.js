920b51137dcb9f183003e1981e4fc42ace7fdb5e
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-02-19 09:12:55 -0500

    spacing for navbar, hover animations for navbar,  and new icons for navbar

--- components/shared/navbar.js
+++ components/shared/navbar.js
@@ -2,6 +2,7 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { motion } from "framer-motion";
 
 export default function Navbar() {
   const router = useRouter();
@@ -12,140 +13,161 @@ export default function Navbar() {
   return (
     <nav className="w-20vw h-screen flex flex-col font-display border-r border-r-borderGray">
       <img
-        className="rounded-full mx-16 my-16"
+        className="rounded-full mx-28 mt-16 mb-12"
         src="/photos/portrait.jpeg"
         alt="profile pic"
       ></img>
 
-      <button className=" w-full pr-16">
-        <div
-          className={`rounded-r-full ${
+      <button className=" w-full pr-28 pb-10">
+        <motion.div
+          className={`rounded-r-full py-3 ${
             router.route === "/dashboard" ? highlightStyling : ""
           }`}
+          whileHover={{
+            color: "rgb(46, 119, 205)",
+          }}
         >
           <h3
-            className={`text-left pl-16  font-bold  ${
+            className={`text-left pl-24  font-semibold text-xl ${
               router.route === "/dashboard" ? textStyling : ""
             }`}
           >
             Dashboard
           </h3>
-        </div>
+        </motion.div>
       </button>
 
-      <hr className="mx-16"></hr>
+      <hr className="mx-24"></hr>
 
-      <ul className="flex flex-col  text-textGray pr-16">
+      <ul className="flex flex-col  text-textGray pr-16 pt-10 space-y-12 pb-10">
         <button className=" w-full">
-          <div
+          <motion.div
             className={`rounded-r-full ${
               router.route === "/breakdown/yearly" ? highlightStyling : ""
             }`}
+            whileHover={{
+              color: "rgb(46, 119, 205)",
+            }}
           >
             <Link href="/breakdown/yearly" passHref>
               <h3
-                className={`text-left pl-16  font-bold  ${
+                className={`text-left pl-24  font-semibold text-xl  ${
                   router.route === "/breakdown/yearly" ? textStyling : ""
                 }`}
               >
                 <a>Yearly Breakdown</a>
               </h3>
             </Link>
-          </div>
+          </motion.div>
         </button>
 
         <button className=" w-full">
-          <div
+          <motion.div
             className={`rounded-r-full ${
               router.route === "/breakdown/yearly" ? highlightStyling : ""
             }`}
+            whileHover={{
+              color: "rgb(46, 119, 205)",
+            }}
           >
             <Link href="/breakdown/monthly" passHref>
               <h3
-                className={`text-left pl-16  font-bold  ${
+                className={`text-left pl-24  font-semibold text-xl  ${
                   router.route === "/breakdown/monthly" ? textStyling : ""
                 }`}
               >
                 <a>Monthly Breakdown</a>
               </h3>
             </Link>
-          </div>
+          </motion.div>
         </button>
 
         <button className=" w-full">
-          <div
+          <motion.div
             className={`rounded-r-full ${
               router.route === "/breakdown/weekly" ? highlightStyling : ""
             }`}
+            whileHover={{
+              color: "rgb(46, 119, 205)",
+            }}
           >
             <Link href="/breakdown/yearly" passHref>
               <h3
-                className={`text-left pl-16 font-bold  ${
+                className={`text-left pl-24 font-semibold text-xl  ${
                   router.route === "/breakdown/weekly" ? textStyling : ""
                 }`}
               >
                 <a>Weekly Breakdown</a>
               </h3>
             </Link>
-          </div>
+          </motion.div>
         </button>
       </ul>
-      <hr className="mx-16"></hr>
+      <hr className="mx-24"></hr>
 
-      <ul className="text-textGray ">
+      <ul className="text-textGray pt-10 space-y-12 pb-10">
         <button className=" w-full">
-          <div
+          <motion.div
             className={`rounded-r-full ${
               router.route === "/goals" ? highlightStyling : ""
             }`}
+            whileHover={{
+              color: "rgb(46, 119, 205)",
+            }}
           >
             <Link href="/goals" passHref>
               <h3
-                className={`text-left pl-16  font-bold  ${
+                className={`text-left pl-24  font-semibold text-xl  ${
                   router.route === "/goals" ? textStyling : ""
                 }`}
               >
                 <a>Your Goals</a>
               </h3>
             </Link>
-          </div>
+          </motion.div>
         </button>
         <button className=" w-full">
-          <div
+          <motion.div
             className={`rounded-r-full ${
               router.route === "/settings" ? highlightStyling : ""
             }`}
           >
             <Link href="/settings" passHref>
               <h3
-                className={`text-left pl-16  font-bold  ${
+                className={`text-left pl-24  font-semibold text-xl  ${
                   router.route === "/settings" ? textStyling : ""
                 }`}
               >
                 <a>Settings</a>
               </h3>
             </Link>
-          </div>
+          </motion.div>
         </button>
         <button className=" w-full ">
-          <div
+          <motion.div
             className={`rounded-r-full ${
               router.route === "/faq" ? highlightStyling : ""
             }`}
+            whileHover={{
+              color: "rgb(46, 119, 205)",
+            }}
           >
             <Link href="/faq" passHref>
               <h3
-                className={`text-left pl-16  font-bold  ${
+                className={`text-left pl-24  font-semibold text-xl  ${
                   router.query === "/faq" ? textStyling : ""
                 }`}
               >
                 <a>FAQ</a>
               </h3>
             </Link>
-          </div>
+          </motion.div>
         </button>
       </ul>
-      <div></div>
+      <div className="flex flex-row-reverse px-20 ">
+        <img src="/icons/questionIcon.svg" className="pl-5"></img>
+        <img src="/icons/settingsIcon.svg"></img>
+      </div>
     </nav>
   );
 }
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,5 +2,12 @@ import React from "react";
 import Navbar from "../components/shared/navbar";
 
 export default function Dashboard() {
-  return <Navbar></Navbar>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="grid ">
+        
+      </div>
+    </div>
+  );
 }
