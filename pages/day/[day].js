921b1defd73f21e67097315792ac2b8bffdf884c
eggy <eggyrules@gmail.com>
2022-02-19 16:18:41 -0500

    Copy slider button thing

--- pages/day/[day].js
+++ pages/day/[day].js
@@ -1,5 +1,7 @@
 import React from "react";
+import { motion } from "framer-motion";
 import spendingData from "../../data/spendingData.json";
+import Navbar from "../../components/shared/navbar";
 
 const allDays = [
   ...new Set(spendingData.map((i) => i.time.substring(0, i.time.indexOf("T")))),
@@ -14,16 +16,31 @@ for (const d of allDays) {
 export default function Day({ day }) {
   return (
     <div>
-      <h2>Purchases</h2>
-      {day.map((d, i) => (
-        <div key={i}>
-          <p>
-            <strong>{d.store}</strong>
-            <p>${d.amount}</p>
-            <p>{new Date(d.time).toLocaleString()}</p>
-          </p>
-        </div>
-      ))}
+      <Navbar />
+      <div className="container grid grid-cols-2 grid-rows-5 w-full p-8">
+        <header className="flex items-center justify-center col-span-2">
+          <motion.button
+            className="rounded-full w-12 h-12 flex items-center justify-center"
+            style={{ backgroundColor: "#E9EFFD" }}
+          >
+            <img src="/icons/leftArrow.svg" alt="left arrow"></img>
+          </motion.button>
+          <h1 className="text-3xl font-bold px-8">February 2022</h1>
+          <motion.button
+            className="rounded-full w-12 h-12 flex items-center justify-center"
+            style={{ backgroundColor: "#F5F3FF" }}
+          >
+            <img src="/icons/rightArrow.svg" alt="right arrow"></img>
+          </motion.button>
+        </header>
+
+        <style jsx>{`
+          .container {
+            margin-left: 25vw;
+            width: 75vw;
+          }
+        `}</style>
+      </div>
     </div>
   );
 }
