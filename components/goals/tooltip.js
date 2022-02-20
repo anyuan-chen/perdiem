ce1bdf35e2c40fddaa6b430978e8071109a5ffdd
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-02-19 20:46:57 -0500

    navbar fixed somewhat - tooltips still broken for 2,3

--- components/goals/tooltip.js
+++ components/goals/tooltip.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { motion } from "framer-motion";
-export default function Tooltip({tooltip}) {
+export default function Tooltip({ tooltip }) {
   return (
     <>
       <motion.div
-        className="pb-8 w-96 px-4 rounded-md absolute"
+        className="pb-8 w-96 px-4 rounded-md fixed"
         id="tooltip-container"
         style={{
           background: "#474747",
-          transform: "transformX(200px)",
           color: "white",
           fontFamily: "Manrope",
           fontSize: "14px",
