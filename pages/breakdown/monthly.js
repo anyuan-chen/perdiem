386196f6918cd76316efcce9542f8922afd701a2
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-02-20 01:44:52 -0500

    fake monthly

--- pages/breakdown/monthly.js
+++ pages/breakdown/monthly.js
@@ -1,80 +1,267 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/shared/navbar";
 import ResultCard from "../../components/breakdown/resultcard";
 import InfoboxSmall from "../../components/breakdown/infobox-small";
 import InfoboxBig from "../../components/breakdown/infobox-big";
-import CompletionBar from "../../components/breakdown/bar";
+import BreakdownBar from "../../components/breakdown/bar";
+import { Line } from "react-chartjs-2";
+import toCsv from "../../components/utils/json2csv";
+import spendingData from "../../data/spendingData.json";
+import dayjs from "dayjs";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
-export default function YearBreakdown() {
-  const monthlyDescription = "August was your best month, with $6,000.20 spent";
-  const weeklyDescription = "Your best week was February 1st to February 8th.";
-  const dailyDescription = "Remember that time you only spent $20 one day in February? I can't.";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default function MonthBreakdown({
+  budget,
+  spent,
+  categories,
+  greatestIncome,
+  lowestStats,
+  username,
+  monthlyTotals,
+  spentTwoMonthsAgo,
+  aboveBudgetDays,
+}) {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  const monthlyDescription = `${
+    lowestStats.month.month
+  } was your best month, with ${formatter.format(
+    lowestStats.month.best
+  )} spent.`;
+  const weeklyDescription = `Your best week was from ${dayjs(
+    lowestStats.week.week
+  ).format("MMMM D")} to ${dayjs(lowestStats.week.week)
+    .add(7, "days")
+    .format("MMMM D")}, when you spent only ${formatter.format(
+    lowestStats.week.best
+  )}.`;
+  const dailyDescription = `Remember that time you only spent ${formatter.format(
+    lowestStats.day.best
+  )} one day in ${dayjs(lowestStats.day.day).format("MMMM")}? I can't.`;
+
+  const lastMonth = dayjs().subtract(1, "month").format("YYYY");
+  const EXAGGERATORY_WORDS = [
+    { multiplier: 1.75, message: "destroyed" },
+    { multiplier: 1.25, message: "were well over" },
+    { multiplier: 0.75, message: "just about met" },
+    { multiplier: 0, message: "were well under" },
+  ];
+
+  let chosenExaggeratoryWord = "you should not see this";
+  for (const pair of EXAGGERATORY_WORDS) {
+    if (pair.multiplier * budget <= spent) {
+      chosenExaggeratoryWord = pair.message;
+      break;
+    }
+  }
+
+  let categoryTotals = {};
+  for (const a of categories) {
+    categoryTotals[a.category] = a.totalMoney;
+  }
+
+  const averages = {
+    monthly: spent / 12,
+    weekly: spent / 52,
+    daily: spent / 365,
+  };
+
+  const lastMonthDeltaPercentage = Math.round(
+    100 - (spent / spentTwoMonthsAgo) * 100
+  );
 
   return (
-    <div className="flex container">
+    <div className="flex container cs-bg">
       <Navbar></Navbar>
-      <div className="flex-column p-20 w-full">
-        <h1 className="title">In 2021, you spent $100,000.04</h1>
+      <div className="flex-column p-20 w-auto" style={{ maxWidth: "1000px" }}>
+        <h1 className="title">
+          In January, you spent {formatter.format(spent)}
+        </h1>
         <h3 className="subtitle">
-          You destroyed your budget of $80,000. Let&apos;s see how you managed
-          to do it.
+          You {chosenExaggeratoryWord} your budget of {formatter.format(budget)}
+          . Let&apos;s see how you managed to do it.
         </h3>
         <br />
         <div className="gap-10 grid grid-flow-row-dense grid-cols-2 grid-rows-5">
-          <InfoboxSmall
-            percentage={16}
-            changePercentage={10}
-            category="Entertainment"
-            totalMoney={12.3}
-          />
-          <InfoboxBig
-            percentage={23}
-            changePercentage={-1}
-            category="Groceries"
-            totalMoney={7892.01}
-          />
-          <InfoboxBig
-            percentage={23}
-            changePercentage={-1}
-            category="Groceries"
-            totalMoney={7892.01}
-          />
-          <InfoboxSmall
-            percentage={16}
-            changePercentage={-10}
-            category="Recreation"
-            totalMoney={9890}
-          />
+          <InfoboxSmall {...categories[0]} />
+          <InfoboxBig {...categories[1]} />
+          <InfoboxBig {...categories[2]} />
+          <InfoboxSmall {...categories[3]} />
         </div>
-        <h1 className="title">Your biggest offenders were...</h1>
-        <div className="bar-container flex flex-col p-6">
-          <CompletionBar
-            percentage={69}
-            description="70% of your groceries were spent at Costco"
-            cost={342.2}
-          />
+        <h1 className="title mt-8">Your biggest offenders were...</h1>
+        <div className="bar-container flex flex-col p-6 bg-white gap-8">
+          {greatestIncome.slice(0, 5).map((obj, i) => (
+            <BreakdownBar
+              key={i}
+              percentage={(obj.total / categoryTotals[obj.category]) * 100}
+              cost={obj.total}
+              description={`${Math.round(
+                (obj.total / categoryTotals[obj.category]) * 100
+              )}% of your ${obj.category.toLowerCase()} spending was at ${
+                obj.name
+              }`}
+            />
+          ))}
         </div>
-        <h1 className="title">On average you spent...</h1>
+        <h1 className="title mt-8">On average you spent...</h1>
         <div className="flex flex-col gap-8">
           <ResultCard
             title="Monthly"
-            money={10000.02}
+            money={averages.monthly}
             description={monthlyDescription}
           />
           <ResultCard
             title="Weekly"
-            money={3333.02}
+            money={averages.weekly}
             description={weeklyDescription}
           />
           <ResultCard
             title="Daily"
-            money={23.02}
+            money={averages.daily}
             description={dailyDescription}
           />
         </div>
-        <div className="bar-container flex justify-center items-center p-6"></div>
+        <br />
+        <br />
+        <div className="bar-container flex flex-col p-6 overlayable bg-white w-full">
+          <h1 className="title">Monthly Spending</h1>
+          <h3 className="subtitle">
+            At the end of 2021, you were{" "}
+            {formatter.format(Math.abs(budget - spent))}{" "}
+            {budget > spent ? "under" : "over"}-budget, or{" "}
+            {Math.round((spent / budget) * 100)}%
+          </h3>
+          <Line
+            datasetIdKey="id"
+            data={{
+              labels: [
+                "Jan",
+                "Feb",
+                "Mar",
+                "Apr",
+                "May",
+                "Jun",
+                "Jul",
+                "Aug",
+                "Sep",
+                "Oct",
+                "Nov",
+                "Dec",
+              ],
+              datasets: [
+                {
+                  id: 1,
+                  label: "",
+                  lineTension: 0.4,
+                  borderColor: "#2E77CD",
+                  data: monthlyTotals,
+                  pointRadius: 0,
+                },
+              ],
+            }}
+            options={{
+              plugins: { legend: { display: false } },
+              scales: {
+                x: {
+                  grid: {
+                    display: false,
+                  },
+                },
+                y: {
+                  display: false,
+                },
+              },
+            }}
+          />
+          <div className="overlayed w-content">
+            <p className="text-sm text-gray-400 font-medium text-right mb-2">
+              budget = {formatter.format(budget)}
+            </p>
+            <p className="text-sm text-gray-400 font-medium text-right">
+              spending = {formatter.format(spent)}
+            </p>
+          </div>
+        </div>
+        <h1 className="title mt-8">Your overall rating is :pensive: 😔</h1>
+        <div className="flex gap-6 p-6 w-full h-80 flex-wrap">
+          <div className="bar-container w-2/5 flex-grow flex flex-col justify-center items-center p-6 bg-white">
+            <p className="uppercase text-center tracking-widest text-gray-900">
+              You were over budget
+            </p>
+            <h4 className="title">
+              {Math.round((aboveBudgetDays / 365) * 100)}%
+            </h4>
+            <h2 className="font-medium text-2xl text-gray-400">of the time</h2>
+          </div>
+          <div className="bar-container w-2/5 flex-grow flex flex-col justify-center items-center p-6 bg-white">
+            <p className="uppercase text-center tracking-widest text-gray-900">
+              You spent
+            </p>
+            <h4 className="title">{Math.abs(lastMonthDeltaPercentage)}%</h4>
+            <h2 className="font-medium text-2xl text-gray-400">
+              {lastMonthDeltaPercentage < 0 ? "less" : "more"} than last month
+            </h2>
+          </div>
+        </div>
+        <p className="font-medium text-lg text-gray-700">
+          {username},{" "}
+          {lastMonthDeltaPercentage > 0
+            ? "you improved! But"
+            : "you haven't improved! And"}
+          … you went {Math.round((spent / budget) * 100)}% over your budget.
+          Sometimes we worry about you. Remember to drink water and take care of
+          your financials.
+        </p>
+        <div className="flex flex-wrap gap-4 justify-between mt-8">
+          <button
+            type="button"
+            className="uppercase text-center font-semibold tracking-widest text-white rounded-md bg-sky-600 p-4 py-2"
+          >
+            Revise your budget
+          </button>
+          <button
+            type="button"
+            className="uppercase text-center font-semibold tracking-widest text-white rounded-md bg-sky-600 p-4 py-2"
+          >
+            See monthly breakdown
+          </button>
+          <a
+            className="uppercase text-center font-semibold tracking-widest text-white rounded-md bg-sky-600 p-4 py-2"
+            href={`data:application/json;charset=utf-8,${encodeURIComponent(
+              toCsv(spendingData)
+            )}`}
+            download="data.csv"
+          >
+            Export daily spending
+          </a>
+        </div>
       </div>
       <style jsx>{`
+        .cs-bg {
+          background: url(/photos/breakdown-bg.svg);
+        }
         .container {
           margin-left: 25vw;
           width: 75vw;
@@ -85,6 +272,10 @@ export default function YearBreakdown() {
           font-size: 2.5rem;
           color: #474747;
           line-height: 5rem;
+        }
+
+        h4.title {
+          font-size: 4rem;
         }
 
         .subtitle {
@@ -98,6 +289,17 @@ export default function YearBreakdown() {
           border-radius: 0.75rem;
         }
 
+        .overlayable {
+          position: relative;
+          display: inline-block;
+        }
+
+        .overlayed {
+          position: absolute;
+          top: 65%;
+          left: 70%;
+        }
+
         * {
           font-family: "Manrope", sans-serif;
         }
@@ -105,3 +307,151 @@ export default function YearBreakdown() {
     </div>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  const httpProtocol = context.req.headers.host.includes("localhost")
+    ? "http"
+    : "https";
+  const { username, budget, balance } = await (
+    await fetch(`${httpProtocol}://${context.req.headers.host}/api/userinfo`)
+  ).json();
+
+  const history = await (
+    await fetch(`${httpProtocol}://${context.req.headers.host}/api/history`)
+  ).json();
+
+  const lastMonth = dayjs().subtract(1, "month").format("YYYY");
+  const lastLastMonth = dayjs().subtract(2, "month").format("YYYY");
+
+  const spent = history
+    .filter((i) => i.time.startsWith(lastMonth))
+    .map((i) => i.amount)
+    .reduce((a, b) => a + b, 0);
+
+  const spentTwoMonthsAgo = history
+    .filter((i) => i.time.startsWith(lastLastMonth))
+    .map((i) => i.amount)
+    .reduce((a, b) => a + b, 0);
+
+  const categories = [];
+
+  for (const c of [
+    "Recreation",
+    "Groceries",
+    "Restaurants",
+    "Transportation",
+    "Other",
+  ]) {
+    const total = history
+      .filter((i) => i.category === c && i.time.startsWith(lastMonth))
+      .map((i) => i.amount)
+      .reduce((a, b) => a + b, 0);
+
+    const totalTwoMonthsAgo = history
+      .filter((i) => i.category === c && i.time.startsWith(lastLastMonth))
+      .map((i) => i.amount)
+      .reduce((a, b) => a + b, 0);
+
+    categories.push({
+      category: c,
+      percentage: (total / spent) * 100 || 0,
+      totalMoney: total || 0,
+      changePercentage: (total / totalTwoMonthsAgo) * 100 || 0,
+    });
+  }
+
+  const greatestIncome = [];
+  const shops = {};
+  history.forEach((i) => {
+    if (!shops[i.name]) shops[i.name] = { name: i.name, category: i.category };
+  });
+
+  for (const i of Object.values(shops)) {
+    greatestIncome.push({
+      name: i.name,
+      category: i.category,
+      total: history
+        .filter((a) => a.name === i.name && a.time.startsWith(lastMonth))
+        .map((a) => a.amount)
+        .reduce((a, b) => a + b, 0),
+    });
+  }
+  greatestIncome.sort((a, b) => b.total - a.total);
+
+  const dayTotals = {};
+  for (const i of history) {
+    const date = dayjs(i.time);
+    const dateFormatted = date.format("YYYY-MM-DD");
+    if (date.month() === +lastMonth && dayTotals[dateFormatted]) {
+      dayTotals[dateFormatted] += i.amount;
+    } else if (date.month() === +lastMonth) {
+      dayTotals[dateFormatted] = i.amount;
+    }
+  }
+
+  const lowestStats = {
+    month: { month: 0, best: 10 ** 7 },
+    week: { week: 0, best: 10 ** 7 },
+    day: { day: 0, best: 10 ** 7 },
+  };
+
+  let currentMonth = 0,
+    currentDay = 0,
+    currentWeek = 0;
+
+  const monthlyTotals = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let aboveBudgetDays = 0;
+  let begin = false;
+
+  for (
+    let day = dayjs(`${lastMonth}-01-01`);
+    day.month() != dayjs().month();
+    day = day.add(1, "day")
+  ) {
+    const dateFormatted = day.format("YYYY-MM-DD");
+    if (dayTotals[dateFormatted]) {
+      currentMonth += dayTotals[dateFormatted];
+      currentWeek += dayTotals[dateFormatted];
+      currentDay = dayTotals[dateFormatted];
+      begin = true;
+    }
+
+    if (day.date() === day.daysInMonth()) {
+      if (lowestStats.month.best > currentMonth && begin) {
+        lowestStats.month.month = day.format("MMMM");
+        lowestStats.month.best = currentMonth;
+      }
+      monthlyTotals[day.month()] = currentMonth;
+      currentMonth = 0;
+    }
+
+    if (day.day() === 6) {
+      if (lowestStats.week.best > currentWeek && begin) {
+        lowestStats.week.week = day.subtract(7, "days").format("YYYY-MM-DD");
+        lowestStats.week.best = currentWeek;
+      }
+      currentWeek = 0;
+    }
+
+    if (lowestStats.day.best > currentDay && begin) {
+      lowestStats.day.day = day.format("YYYY-MM-DD");
+      lowestStats.day.best = currentDay;
+    }
+
+    if (currentDay > budget / 365) aboveBudgetDays++;
+  }
+
+  return {
+    props: {
+      budget,
+      spent,
+      categories,
+      greatestIncome,
+      lowestStats,
+      username,
+      monthlyTotals,
+      spentTwoMonthsAgo,
+      aboveBudgetDays,
+    },
+  };
+};
